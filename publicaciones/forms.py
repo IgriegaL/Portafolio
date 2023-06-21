6a8c178a2fad7f6544ace2f8abaa74a7443f86ae
IgriegaL <yehudilara@hotmail.com>
2023-06-21 19:45:12 -0400

    fix para mostrar tabla afinnity

--- publicaciones/forms.py
+++ publicaciones/forms.py
@@ -1,30 +1,20 @@
 from django import forms
 from django.forms.widgets import DateInput
 
-from login.models import Product
+from login.models import Affinity, Product
 
+AFFINITY_CHOICES = [(affinity.af_id, affinity.af_name) for affinity in Affinity.objects.all()]
 
 class CustomDateInput(DateInput):
     input_type = 'date'
     format = '%Y-%m-%d'
     input_formats = ['%Y-%m-%d']
 
-
 class ProductForm(forms.ModelForm):
+    prod_affinitie1 = forms.ChoiceField(choices=AFFINITY_CHOICES)
+    prod_affinitie2 = forms.ChoiceField(choices=AFFINITY_CHOICES)
 
     class Meta:
         model = Product
-        fields = [
-            'prod_name', 
-            'prod_new', 
-            'permuta', 
-            'prod_price', 
-            'prod_description',
-            'prod_affinitie1', 
-            'prod_affinitie2',
-            'prod_photo1', 
-            'prod_photo2', 
-            'prod_photo3', 
-            'prod_photo4', 
-            'prod_photo5'
-        ]
+        fields = ['prod_name', 'prod_new', 'permuta', 'prod_price', 'prod_description', 'prod_affinitie1',
+                  'prod_affinitie2', 'prod_photo1', 'prod_photo2', 'prod_photo3', 'prod_photo4', 'prod_photo5']
--- publicaciones/views.py
+++ publicaciones/views.py
@@ -1,7 +1,7 @@
 import datetime
 from pyexpat.errors import messages
 from django.shortcuts import redirect, render
-from login.models import User
+from login.models import User, Affinity
 import uuid
 from .forms import ProductForm
 
@@ -13,13 +13,21 @@ def create_product(request):
     user = User.objects.get(user_id=user_id)
     print(user)
     form = ProductForm(request.POST, request.FILES or None)
+    print(request.POST)
     if form.is_valid():
         product = form.save(commit=False)
         product.prod_id = uuid.uuid4().hex
         product.prod_seller = user
         product.prod_date = datetime.date.today().isoformat()
         product.prod_active = True
+        affinitie1_id = form.cleaned_data["prod_affinitie1"]
+        affinitie2_id = form.cleaned_data["prod_affinitie2"]
         
+        affinitie1 = Affinity.objects.get(pk=affinitie1_id)
+        affinitie2 = Affinity.objects.get(pk=affinitie2_id)
+        
+        product.prod_affinitie1 = affinitie1.af_id
+        product.prod_affinitie2 = affinitie2.af_id
         product.save()
         return redirect('home')
     context = {'form': form, 'hide_footer': True}
